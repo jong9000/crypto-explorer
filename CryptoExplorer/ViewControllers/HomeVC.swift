dd7179c5238665821f4ae5afb99f61bc034900a4
Jon Gentry <jonmarkgentry@gmail.com>
2021-07-16 11:55:46 -1000

    Create basic networking manager.

--- CryptoExplorer/ViewControllers/HomeVC.swift
+++ CryptoExplorer/ViewControllers/HomeVC.swift
@@ -12,6 +12,7 @@ class HomeVC: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemPink
+    NetworkManager.shared.getTrending()
   }
   
 }
